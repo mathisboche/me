9710d039cbec7d55f4e96a0ccd65663e4a52899b
mathisboche <141416601+mathisboche@users.noreply.github.com>
2024-01-01 04:32:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// Code JavaScript de base pour l'interaction (à développer selon les besoins)
+function toggleMenu() {
+    var menu = document.getElementById("menu-principal");
+    menu.classList.toggle("active");
+
+    let links = document.querySelectorAll('#menu-principal a');
+
+    if (menu.classList.contains("active")) {
+        links.forEach((link, index) => {
+            link.style.opacity = "0";
+            link.style.animation = `fadeInDown 0.5s ease forwards ${index * 100 + 300}ms`;
+        });
+    } else {
+        links.forEach(link => {
+            link.style.opacity = "0";
+            link.style.animation = '';
+        });
+    }
+}
+
+document.querySelectorAll('#menu-principal a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault(); // Empêcher le comportement par défaut du lien
+        const href = link.getAttribute('href');
+        const section = document.querySelector(href);
+
+        // Fermer le menu avant de commencer le défilement
+        toggleMenu();
+
+        // Attendre la fin de l'animation du menu avant de défiler
+        setTimeout(() => {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }, 500); // Ajustez ce délai si nécessaire
+    });
+});
