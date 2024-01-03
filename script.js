9c0178e33847a808626e3688785ae112b846fc9c
mathisboche <141416601+mathisboche@users.noreply.github.com>
2024-01-03 06:05:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ function toggleMenu() {
             link.style.opacity = "0";
             link.style.animation = `fadeInDown 0.5s ease forwards ${index * 100 + 300}ms`;
         });
+        document.body.style.overflow = 'hidden';
     } else {
         links.forEach(link => {
             link.style.opacity = "0";
             link.style.animation = '';
         });
+        document.body.style.overflow = '';
     }
 }
 
@@ -27,8 +29,39 @@ document.querySelectorAll('#menu-principal a').forEach(link => {
         toggleMenu();
 
         // Attendre la fin de l'animation du menu avant de défiler
-        setTimeout(() => {
+        /*setTimeout(() => {
             section.scrollIntoView({ behavior: 'smooth' });
-        }, 500); // Ajustez ce délai si nécessaire
+        }, 100); // Ajustez ce délai si nécessaire*/
+        
+        section.scrollIntoView({ behavior: 'smooth' });
+        
     });
 });
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+    setTimeout(() => {
+        modal.classList.add('show');
+    }, 10);
+    document.body.style.overflow = 'hidden'; // Désactive le défilement
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.classList.remove('show');
+    modal.addEventListener('transitionend', () => {
+        modal.style.display = 'none';
+    }, { once: true });
+    document.body.style.overflow = ''; // Réactive le défilement
+}
+
+// Ferme le modal lorsqu'on clique en dehors
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach((modal) => {
+        if (event.target === modal) {
+            closeModal(modal.id);
+        }
+    });
+}
