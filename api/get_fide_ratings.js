21f4bba73e3b530ddca8c4fd4c85aeae1243c339
mathisboche <bibi_76@outlook.fr>
2024-06-27 15:06:42 +0200

    Ajout de l'API pour récupérer les Élos FIDE

---
+++ api/get_fide_ratings.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+  try {
+    const response = await axios.get('https://ratings.fide.com/profile/651010240');
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const standard = $('.profile-top-rating-data_gray').text().trim().replace('std', '');
+    const rapid = $('.profile-top-rating-data_red').text().trim().replace('rapid', '');
+    const blitz = $('.profile-top-rating-data_blue').text().trim().replace('blitz', '');
+
+    res.status(200).json({ standard, rapid, blitz });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+  }
+};
